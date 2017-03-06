bc210eb5e3c06533a15491f1122400b814917d51
XiongAmao <onycat@live.cn>
2017-03-06 23:10:48 +0800

    整理过文档内容，删掉一些不必要的内容，服务器主脚本为index.js

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ var server = http.createServer(function(request, response){
 
   //从这里开始看，上面不要看
 
-if(path === '/brendan_eich'){
+if(path === '/'){
   var htmlString = fs.readFileSync('./index.html') 
   response.end(htmlString)
 }else if(path === '/index.css'){
   var cssString = fs.readFileSync('./index.css')
   response.setHeader('Content-Type','text/css;charset=utf-8')
   response.end(cssString)
-}else if(path === '/firstweb.js'){
-  var jsString = fs.readFileSync('./firstweb.js')
-  response.setHeader('Content-Type','text/javascript')
+}else if(path === '/main.js'){
+  var jsString = fs.readFileSync('./main.js')
+  response.setHeader('Content-Type','application/javascript')
   response.end(jsString)
 }else{
   response.end('404')
